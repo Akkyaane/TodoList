3253dad807fb0cf81ac18e34b88f3e9e36765b31
eliottdelanneeGIT <eliott.delannee@gmail.com>
2025-11-24 15:15:54 +0100

    Update register.php

--- public/register.php
+++ public/register.php
@@ -1,1 +1,5 @@
-<?php echo 'register';
+<form method="POST" action="/register">
+    <input type="email" name="email" required placeholder="Email">
+    <input type="password" name="password" required placeholder="Mot de passe">
+    <button type="submit">Créer un compte</button>
+</form>
